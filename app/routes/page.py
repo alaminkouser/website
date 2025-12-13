a36c175999a9f72702dc6b255bdaae9bbcb3c138
Al Amin Kouser <alaminkouser@icloud.com>
2025-12-13 22:06:59 +0600

    refactor: manage cache control headers directly in the application code and remove Vercel configuration.

--- app/routes/page.py
+++ app/routes/page.py
@@ -5,6 +5,8 @@ from fastapi.responses import FileResponse, Response
 
 page_router = APIRouter()
 
+CACHE_CONTROL_HEADER = {"Cache-Control": "public, max-age=3600"}
+
 
 def not_found(request: Request):
     html_string = home.get_template("templates/404.html").render(request=request)
@@ -22,15 +24,17 @@ def page(request: Request, path: str):
         and os.path.isfile(f"app/home{path}index.html")
     ):
         html_string = home.get_template(f"{path}index.html").render(request=request)
-        return Response(content=html_string, media_type="text/html")
+        return Response(
+            content=html_string,
+            media_type="text/html",
+            headers=CACHE_CONTROL_HEADER if path.startswith("/docs/") else None,
+        )
 
     if (
         not path.endswith(".html")
         and os.path.exists(f"app/home{path}")
         and os.path.isfile(f"app/home{path}")
     ):
-        return FileResponse(
-            f"app/home{path}", headers={"Cache-Control": "public, max-age=3600"}
-        )
+        return FileResponse(f"app/home{path}", headers=CACHE_CONTROL_HEADER)
 
     return not_found(request)
