86a4725d0035246b785a6faa010e64f71c2ebd91
AL AMIN KOUSER <alaminkouser@icloud.com>
2026-02-04 21:15:35 +0600

    fixed: image src

--- docs/src/authorConfig/authorList.ts
+++ docs/src/authorConfig/authorList.ts
@@ -1,10 +1,11 @@
 import type { StarlightBlogConfig } from "starlight-blog";
+import alaminkouser from "./pictures/alaminkouser.png";
 
 const authorList: StarlightBlogConfig["authors"] = {
   alaminkouser: {
     name: "AL AMIN KOUSER",
     title: "Software Engineer & Maritime Researcher",
-    picture: "./src/authorConfig/pictures/alaminkouser.png",
+    picture: alaminkouser.src,
     url: "https://alaminkouser.com/",
   },
 };
