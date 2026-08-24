5cbc75607361d48731c9003cd459e7edfd04d509
AL AMIN KOUSER <alaminkouser@icloud.com>
2026-08-24 18:25:12 +0600

    feat(account): add basic account page and form handling

--- app/routes/account/index.py
+++ app/routes/account/index.py
@@ -1,8 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Response, Form
+from typing import Annotated
+from app.tools.home import home
 
 router_account = APIRouter()
 
 
 @router_account.get("/")
-async def account_get():
-    return "ACCOUNT ROOT"
+async def account_get(request: Request) -> Response:
+    html_string = home.get_template("account/index.html").render(request=request)
+    return Response(
+        content=html_string,
+        media_type="text/html",
+    )
+
+
+@router_account.post("/")
+async def account_post(email: Annotated[str, Form()]) -> Response:
+    return Response(
+        content=email,
+        media_type="text/html",
+    )
